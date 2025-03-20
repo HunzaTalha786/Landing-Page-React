1bd64d52dc823b51d82fd84cd7b6896a293c934a
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-03-20 21:39:13 +0500

    Done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = ({ myTheme }) => {
   return (
     <footer className="--flex-center" data-theme={myTheme}>
-      <p>Copyright &copy; 2022.</p>
+      <p>Copyright &copy; 2025.</p>
     </footer>
   );
 };
